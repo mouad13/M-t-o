4851c1871549a35cc9bb270f96a5c1fb41f41a8c
mouad13 <mouad.rifaielmoubarik@gmail.com>
2017-04-12 16:21:52 +0200

    Meteo

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 $.ajax({
 
-url : 'http://api.openweathermap.org/data/2.5/weather?id=6448311&APPID=6710107f875ce48689449d2f02f2e715',
+url : 'http://api.openweathermap.org/data/2.5/weather?id=6448311&APPID=6710107f875ce48689449d2f02f2e715&mode=html',
   
   success : function(data){
 
     console.log(data);
     console.log('success');
+    $("#koko").append(data);
   },
 
   error : function(error){
@@ -14,3 +15,6 @@ url : 'http://api.openweathermap.org/data/2.5/weather?id=6448311&APPID=6710107f8
   },
 
 });
+
+
+
